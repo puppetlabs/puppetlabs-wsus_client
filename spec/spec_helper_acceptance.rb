ccec99ea951b01f47a1a43430d03e4d3fe88acab
Travis Fields <travis@puppetlabs.com>
2015-08-18 16:11:08 -0700

    (maint) Add install_ca_certs
    - Start downloading the stable version from forge now that we
      install the certificates that are required for the forge

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -2,6 +2,7 @@ require 'beaker-rspec'
 require 'beaker/puppet_install_helper'
 
 run_puppet_install_helper
+install_ca_certs_on default
 
 RSpec.configure do |c|
   # Readable test descriptions
@@ -13,9 +14,8 @@ unless ENV['MODULE_provision'] == 'no'
   result = on default, "echo #{default['distmoduledir']}"
   target = result.raw_output.chomp
   proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-  {'stdlib' => '4.6.0', 'registry' => '1.1.0'}.each do |repo, version|
-    on default, "rm -rf #{target}/#{repo};git clone --branch #{version} --depth 1 https://github.com/puppetlabs/puppetlabs-#{repo} #{target}/#{repo}"
-  end
+  on default, puppet("module install puppetlabs-stdlib")
+  on default, puppet("module install puppetlabs-registry")
   install_dev_puppet_module_on(default, {:proj_root => proj_root, :target_module_path => "#{target}", :module_name => 'wsus_client'})
 end
 
